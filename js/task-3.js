c4e9da5ce1819962fa64b3fae6f78608432db56f
oliyka <olja.x.06@gmail.com>
2025-05-10 13:10:40 +0300

    added

--- js/task-3.js
+++ js/task-3.js
@@ -1,31 +1,295 @@
-// Змінна num може набувати 4 значення: '1', '2', '3' або '4'
-// (запитуй це значення у користувача через prompt).
-// Якщо вона має значення '1', то у змінну result запишіть 'зима',
-// якщо має значення '2' - 'весна' і так далі.
-// Розв'яжіть завдання через switch-case.
-// Не забудьте про дефолтне значення, на випадок, якщо користувач
-// введе в prompt щось інше. В такому випадку result має набувати значення:
-// "Вибачте, але ви маєте ввести значення від 1 до 4 включно".
-// Значення змінної result виведіть в консоль.
+// task-1
+// Дано масив чисел [1, 2, 3, 4, 5]. Створіть новий масив, що містить
+// квадрати кожного елементу вхідного масиву.
+// Очікуваний результат: [1, 4, 9, 16, 25].
 
-// let num = +prompt('Введіть значення');
-// let result = "";
+// const numbers = [1, 2, 3, 4, 5]
 
-// switch (num) {
-//     case 1:
-//         result = "Зима";
-//         break;
-//     case 2:
-//         result = "Весна";
-//         break;
-//     case 3:
-//         result = "Літо";
-//         break;
-//     case 4:
-//         result = "Осінь";
-//         break;
-//     default:
-//         result = "Вибачте, але ви маєте ввести значення від 1 до 4 включно";
+// const newNumbers = numbers.map(number => number * number);
+
+// console.log(newNumbers)
+
+
+
+// task-2
+// Дано масив об'єктів. Створіть новий масив, що містить всі значення
+// з масивів values кожного об'єкту, збережених в одному масиві.
+// Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+// const data = [
+//  { id: 1, values: [1, 2, 3] },
+//  { id: 2, values: [4, 5, 6] },
+//  { id: 3, values: [7, 8, 9] },
+// ];
+
+// const newData = data.flatMap(item => item.values);
+
+// console.log(newData)
+
+
+
+// task-3
+// Дано масив об'єктів.
+// Перевірте, чи є хоча б один об'єкт з віком менше 20 років.
+// Очікуваний результат: true.
+
+// const people = [
+//  { name: 'John', age: 27 },
+//  { name: 'Jane', age: 31 },
+//  { name: 'Bob', age: 19 },
+// ];
+
+// const checkAge = people.some(person => person.age < 20);
+// console.log(checkAge)
+
+
+
+// task-4
+// Дано масив чисел [2, 4, 6, 8, 10].
+// Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
+
+// const numbers = [2, 4, 6, 8, 10];
+
+// const filtNumbers = numbers.every(num => num % 2 == 0);
+// console.log(filtNumbers)
+
+
+
+// task-5
+// Знайдіть перше непарне число
+
+// const numbers = [2, 1, 6, 8, 9, 10, 12];
+// const findNum = numbers.find(item => item % 2 !== 0);
+// console.log(findNum)
+
+
+
+// task-6
+// Відсортуйте масив чисел [4, 2, 5, 1, 3]
+// у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
+
+// const numbersArray = [4, 2, 5, 1, 3];
+// const filtNumArr = numbersArray.toSorted();
+// console.log(filtNumArr)
+
+
+
+// task-7
+// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"]
+//  у порядку алфавіту.
+// Очікуваний результат: ["apple", "banana", "orange", "pear"].
+
+// const stringArray = ['banana', 'orange', 'apple', 'pear'];
+
+// const sortStringArr = stringArray.toSorted();
+// console.log(sortStringArr);
+
+
+
+// task-8
+//  Відсортуйте масив об'єктів за віком у порядку зростання.
+// Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
+
+// const users = [
+//  { name: 'John', age: 27 },
+//  { name: 'Jane', age: 31 },
+//  { name: 'Bob', age: 19 },
+// ];
+
+// const sortUsers = users.toSorted((firstUs, secondUs) => firstUs.age - secondUs.age);
+// console.log(sortUsers);
+
+
+
+// task-9
+// Дано масив об'єктів. 
+// Створіть новий масив, що містить тільки об'єкти, в яких 
+// вік більше 20 років. 
+// Очікуваний результат: [{name: "John", age: 27}, {name: "Jane", age: 31}]
+
+// const user = [
+//  { name: 'John', age: 27 },
+//  { name: 'Jane', age: 31 },
+//  { name: 'Bob', age: 19 },
+// ]
+
+// const filtUsers = user.filter(user => user.age > 20);
+// console.log(filtUsers);
+
+
+
+// task-10
+// Дано масив чисел [1, 2, 3, 4, 5].
+// Застосуйте метод для обчислення суми елементів масиву.
+
+// const numbers = [1, 2, 3, 4, 5];
+// const sum = numbers.reduce((acc, num) => acc + num, 0);
+
+// console.log(sum)
+
+
+// task-11
+ // Розроби клас Calculator, який дозволяє виконувати арифметичні 
+ //  операції над числом за допомогою методів класу, підтримуючи  
+ // ланцюжковий виклик (method chaining).
+ //
+ // Вимоги до класу Calculator
+ // - Метод number(value)
+ // Встановлює початкове значення для наступних обчислень.
+ // Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+ 
+ // - метод getResult, Повертає поточний результат усіх операцій.
+ // Не змінює значення, просто повертає його.
+ 
+ // - метод add - Додає value до поточного значення.
+ // Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+ 
+ // - метод substruct - Віднімає value від поточного значення. Повертає this.
+ 
+// - метод divide - Ділить поточне значення на value, якщо value не дорівнює 0.
+// Якщо value === 0, викидає помилку з повідомленням про неможливість ділення.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+ 
+ // - метод multiply -Множить поточне значення на value. Повертає this.
+ 
+ // Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
+ 
+ // Приклад використання:
+ // const calc = new Calculator();
+
+
+// const result = calc
+//   .number(10)   // Встановлюємо початкове значення 10
+//   .add(5)       // Додаємо 5 (10 + 5 = 15)
+//   .subtract(3)  // Віднімаємо 3 (15 - 3 = 12)
+//   .multiply(4)  // Множимо на 4 (12 * 4 = 48)
+//   .divide(2)    // Ділимо на 2 (48 / 2 = 24)
+//   .getResult(); // Отримуємо результат: 24
+
+
+//   console.log(result); // 24
+
+// class Calculator {
+//     constructor() {
+//         this.currentValue = 0;
+//     }
+//     number(value) {
+//         this.currentValue = value;
+//         return this;
+//     }
+//     getResult() {
+//         return this.currentValue;
+//     }
+//     add(value) {
+//         this.currentValue += value;
+//         return this;
+//     }
+//     substruct(value) {
+//         this.currentValue -= value;
+//         return this;
+//     }
+//     divide(value) {
+//         if (value === 0) {
+//             console.log("Неможливо ділити на нуль");
+//             return this;
+//           }
+//         this.currentValue /= value;
+//         return this;
+//     }
+//     multiply(value) {
+//         this.currentValue *= value;
+//         return this;
+//     }
 // }
 
-// console.log(result);
+// const calc = new Calculator()
+
+// const result = calc
+// .number(10)
+// .add(5)
+// .substruct(3)
+// .multiply(4)
+// .divide(0)
+// .getResult(); 
+
+// console.log(result)
+
+
+
+// task-12
+ // Напиши клас Client який створює об'єкт з властивостями login email.
+ // Оголоси приватні властивості #login #email, доступ до яких зроби 
+ // через геттер та сеттер login email
+ 
+// class Client{
+//     #login
+//     #email
+//     constructor(login, email) {
+//         this.#login = login;
+//         this.#email = email;
+//     }
+//     get login() {
+//         return this.#login;
+//     }
+//     set login(newLogin) {
+//         return this.#login = newLogin;
+//     }
+//     get email() {
+//         return this.#email;
+//     }
+//     set email(newEmail) {
+//         return this.#email = newEmail;
+//     }
+    
+//  }
+
+
+
+// task-13
+  //  Наслідування у класах!
+  // Cтворіть клас `Person`, який містить наступні властивості:
+  //  - `name` - ім'я людини;
+  //  - `age`- вік людини;
+  //  - `gender` - стать людини;
+  //  - `email`- електронна пошта людини.
+  
+  // Крім того, клас `Person` має мати метод `getDetails()`,
+  // який повертає об'єкт з ім'ям, віком, статтю 
+  //та електронною поштою людини.
+  
+  // 
+  // Потім Створіть клас `Employee`, який розширює клас `Person` і містить наступні властивості:
+  //  - salary - зарплата співробітника;
+  //  - department - відділ, в якому працює співробітник.
+  // Крім того, клас `Employee` має мати метод `getEmployeeDetails()`, який повертає об'єкт з зарплатою співробітника та відділом, в якому він працює.
+
+// class Person {
+//     constructor(name, age, gender, email) {
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//         this.email = email;
+//     }
+//     getDetails() {
+//         return {
+//             name: this.name,
+//             age: this.age,
+//             gender: this.gender,
+//             email: this.email
+//         };
+//     }
+// }
+
+// class Employee extends Person{
+//     constructor(name, age, gender, email, salary, department) {
+//         super(name, age, gender, email);
+//         this.salary = salary;
+//         this.department = department;
+//     }
+//     getEmployeeDetails(){
+//         return {
+//             salary: this.salary,
+//             department: this.department
+//         };
+//     }  
+// }
--- js/task-8.js
+++ js/task-8.js
@@ -1,15 +1,1 @@
-// Напишіть функцію min(a, b), яка приймає 2 числа і повертає меньше з них.
-// Потрібно додати перевірку, що функція отримує саме числа, в іншому випадку
-// повертати з функції рядок - 'Not a number!'.
 
-// function min(a, b) {
-//     if (typeof a == 'number' && typeof b == 'number') {
-//         return Math.min(a, b);
-//     }
-//     return 'Not a number!';
-// }
-
-// console.log(min(2, 5));     // 2
-// console.log(min(10, 3));    // 3
-// console.log(min(-1, -5));   // -5
-// console.log(min('a', 5));   // 'Not a number!'
